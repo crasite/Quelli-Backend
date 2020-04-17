60f27d0dbcd7bcdb0d571845ff0241cfae303a95
Paichana Kularb <paichana1@gmail.com>
2020-04-17 22:13:49 +0700

    add mongo

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 VERSION=0.0.1
 
 serve:
-	STAGE=production node app.js
+	MONGODB_URI=mongodb://localhost:27017 STAGE=production node app.js
+
+mongo:
+	docker run -p 27017:27017 mongo  
 
 docker-run:
 	docker run -p 3000:3000 quelli-backend:${VERSION}
--- app.js
+++ app.js
@@ -6,47 +6,59 @@ const errorhandler = require('errorhandler')
 const app = express()
 const port = 3000
 
+
 if (!isProduction) {
     app.use(errorhandler());
 }
 
+if (isProduction) {
+    mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true });
+} else {
+    mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true })
+    mongoose.set('debug', true);
+}
+
 var isProduction = process.env.STAGE === 'production'
-console.log('Serving on stage: '+process.env.STAGE)
+console.log('Serving on stage: ' + process.env.STAGE)
 
 //Add routes
 app.use(require('./routes'))
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found')
     err.status = 404
     next(err)
-  })
+})
 
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-    app.use(function(err, req, res, next) {
-      console.log(err.stack)
-  
-      res.status(err.status || 500)
-  
-      res.json({'errors': {
-        message: err.message,
-        error: err
-      }})
-    })
-  }
-  
-  // production error handler
-  // no stacktraces leaked to user
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500)
-    res.json({'error': {
-      message: "OOPS Internal Server Error :C my bad..."
-    }})
-  })
+    app.use(function (err, req, res, next) {
+        console.log(err.stack)
 
-  var server = app.listen( process.env.PORT || port, function(){
+        res.status(err.status || 500)
+
+        res.json({
+            'errors': {
+                message: err.message,
+                error: err
+            }
+        })
+    })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
+        'error': {
+            message: "OOPS Internal Server Error :C my bad..."
+        }
+    })
+})
+
+var server = app.listen(process.env.PORT || port, function () {
     console.log('Listening on port ' + server.address().port)
-  })
+})
